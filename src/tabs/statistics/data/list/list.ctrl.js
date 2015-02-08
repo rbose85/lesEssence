e2d560a8a219cd554b175b3828a5a72299e6c388
Rajiv Bose <rajiv@lesbose.com>
2015-02-08 14:42:37 +0000

    add support to delete a previous recorded tank of gas

--- src/tabs/statistics/data/list/list.ctrl.js
+++ src/tabs/statistics/data/list/list.ctrl.js
@@ -5,10 +5,42 @@
         .controller('TabStatisticsDataListController', TabStatisticsDataListCtrl);
 
     /* @ngInject */
-    function TabStatisticsDataListCtrl(previousTanks) {
+    function TabStatisticsDataListCtrl($ionicPopup, previousTanks, spinner) {
         var vm = this;
 
         vm.tanks = previousTanks;
+
+        vm.edit = function (tank) {
+            console.log('--rb-- edit(tank): ', tank);
+        };
+
+        var deleteTank = function (tank) {
+            spinner.show();
+
+            vm.tanks.$remove(tank)
+                .then(function () {
+                    spinner.timeout('Success!');
+                })
+                .catch(function (error) {
+                    console.error(angular.toJson(error, true));
+                    spinner.error('Unable to delete Tank.');
+                });
+        };
+
+        vm.remove = function (tank) {
+            console.log('--rb-- delete(tank): ', tank);
+
+            $ionicPopup.confirm({
+                title: 'D E L E T E',
+                template: 'Are you sure you want to delete this record?'
+            })
+                .then(function (isConfirmed) {
+                    if (isConfirmed) {
+                        deleteTank(tank);
+                    }
+                });
+        };
+
     }
 
 })();
