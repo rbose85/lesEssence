51b856da5a8bed8f66a28cd858339fc1fb157dfa
Rajiv Bose <rajiv@lesbose.com>
2015-02-09 22:04:04 +0000

    fix to missing spinner icon

--- src/utils/common/spinner.js
+++ src/utils/common/spinner.js
@@ -6,7 +6,7 @@
 
     /* @ngInject */
     function SpinnerService($ionicLoading, $timeout) {
-        var spinnerIcon = '<i class="icon ion-loading-c"></i> ';
+        var spinnerIcon = '<i class="icon ion-load-c"></i> ';
 
         var show = function (message) {
             message = message || spinnerIcon;
