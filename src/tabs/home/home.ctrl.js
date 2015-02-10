d22b7053a04483fd4d72f1f3e6a63019560d0d09
Rajiv Bose <rbose85@users.noreply.github.com>
2015-02-10 10:22:03 +0000

    remove unused logic from TabsHomeController

--- src/tabs/home/home.ctrl.js
+++ src/tabs/home/home.ctrl.js
@@ -5,21 +5,7 @@
         .controller('TabsHomeController', TabsHomeCtrl);
 
     /* @ngInject */
-    function TabsHomeCtrl($ionicHistory, $ionicNavBarDelegate, $state) {
-        var vm = this;
-
-        vm.goToHistoryAdd = function () {
-            $ionicHistory.nextViewOptions({
-                disableAnimate: false,
-                disableBack: false
-            });
-            $ionicNavBarDelegate.showBackButton(true);
-
-
-            // NEED TO MANUALLY ENTER BACK STATE INTO STACK!
-            $state.go('tabs.statistics.history.add', {backlocation: 'tabs.home'});
-            //$state.go('');
-        };
+    function TabsHomeCtrl() {
     }
 
 })();
