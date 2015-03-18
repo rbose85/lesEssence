14628ea62034595630c8567eacdd87c9314a0db5
Liam Warby <github@liamwarby.co.uk>
2015-03-18 20:21:36 +0000

    Finalises styles prior to SASSification

--- src/app.filter.js
+++ src/app.filter.js
@@ -2,12 +2,23 @@
     'use strict';
 
     angular.module('app')
-        .filter('dashcase', ToDashCaseFilter);
+        .filter('dashcase', ToDashCaseFilter)
+        .filter('todecimal', toDecimal);
 
     /* @ngInject */
     function ToDashCaseFilter() {
         return function (items) {
             return items && items.replace(/\.+/g, '-');
+        };
+    }
+
+    function toDecimal () {
+        return function (input, places) {
+            if (isNaN(input)) return input;
+            // If we want 1 decimal place, we want to mult/div by 10
+            // If we want 2 decimal places, we want to mult/div by 100, etc
+            // So use the following to create that factor
+            var factor = "1" + Array(+(places > 0 && places + 1)).join("0");
         };
     }
 
